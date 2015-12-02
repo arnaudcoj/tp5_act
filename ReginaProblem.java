c4d1f29060f85bcc07d3c6524e35d86fb4cf952d
radgeur <remy2112@hotmail.fr>
2015-12-02 09:15:20 +0100

    update ReginaProblem + partie 2 finished

--- ReginaProblem.java
+++ ReginaProblem.java
@@ -10,12 +10,20 @@ public class ReginaProblem {
   }
 
   public boolean isCorrect(CertificatPizza cert) {
+    boolean verif[][] = new boolean[pizza.length][pizza[0].length];
+    if (n<0)
+      return false;
     for(TripletPizza triplet : cert.getParts()) {
       int cptJambon = 0;
-      if(triplet.getWidth() * triplet.getHeight() > this.c)
+      int partSize = triplet.getWidth() * triplet.getHeight();
+      if(partSize > this.c || partSize <= 0)
         return false;
       for (int i = triplet.getX(); i< triplet.getWidth() + triplet.getX(); i++) {
         for (int j = triplet.getY(); j < triplet.getHeight() + triplet.getY(); j++) {
+          if (!verif[i][j])
+            verif[i][j] = true;
+          else
+            return false;
           if (this.pizza[i][j] == Garniture.H)
             cptJambon++;
         }
